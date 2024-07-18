5f7f2abdc460969a55c9cf93c322504f3f03fe73
mallampati-sumanth <msumanth117@gmail.com>
2024-07-18 18:15:23 +0530

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navMenu = document.getElementById('nav-menu');
+    const navToggle = document.getElementById('nav-toggle');
+    const navClose = document.getElementById('nav-close');
+    const navLinks = document.querySelectorAll('.nav__link');
+
+    if (navToggle) {
+        navToggle.addEventListener('click', () => {
+            navMenu.classList.add('show-menu');
+        });
+    }
+
+    if (navClose) {
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu');
+        });
+    }
+
+    function linkAction() {
+        navMenu.classList.remove('show-menu');
+    }
+
+    navLinks.forEach(link => link.addEventListener('click', linkAction));
+});
